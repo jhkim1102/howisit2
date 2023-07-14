7fc9d8eedf8f387ab2ff01057dedd6a956b89f80
endendrh222 <endendrh222@naver.com>
2023-07-14 09:39:21 +0900

    up

--- src/main/java/com/howisit/config/WebMvcConfig.java
+++ src/main/java/com/howisit/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	String uploadPath = "file:///C:/howisit/"; //업로드할 경로
+	String uploadPath = "file:///C:/shop/"; //업로드할 경로
 	
 	//웹 브라우저에서 URL이 /images로 시작하는 경우 uploadPath에 설정한 폴더를 기준으로 파일을 읽어온다.
 	@Override
